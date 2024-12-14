cb2ccb140f1aacd1dd3487e5e0b2f151f181437e
oklmcmoi <oklmcrypto@gmail.com>
2024-12-14 16:45:12 +0100

    Update game configuration and integrate Phaser game

--- src/web/components/AppTest.jsx
+++ src/web/components/AppTest.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import { Game } from './Game';
 
 const App = () => {
     return (
         <div className="app-container">
-            <div className="container py-4">
-                <h1 className="text-center">MangaMaze</h1>
-                <p className="text-center">Bienvenue dans le jeu !</p>
+            <div className="container-fluid p-0">
+                <Game />
             </div>
         </div>
     );
--- src/web/game/config.js
+++ src/web/game/config.js
@@ -5,10 +5,11 @@ import { GameScene } from './scenes/GameScene';
 import { PreloadScene } from './scenes/PreloadScene';
 
 export const gameConfig = {
-    type: Phaser.WEBGL,
+    type: Phaser.AUTO,
     parent: 'game-container',
     width: 1920,
     height: 1080,
+    backgroundColor: '#000000',
     physics: {
         default: 'arcade',
         arcade: {
@@ -31,44 +32,6 @@ export const gameConfig = {
     },
     scene: [PreloadScene, LoadingScene, MenuScene, GameScene],
     pixelArt: false,
-    backgroundColor: '#000000',
-    render: {
-        antialias: true,
-        pixelArt: false,
-        roundPixels: false,
-        transparent: false,
-        clearBeforeRender: true,
-        premultipliedAlpha: true,
-        preserveDrawingBuffer: true,
-        failIfMajorPerformanceCaveat: false,
-        powerPreference: 'high-performance',
-        batchSize: 4096
-    },
-    fps: {
-        target: 60,
-        forceSetTimeOut: false,
-        deltaHistory: 10
-    },
-    callbacks: {
-        postBoot: function (game) {
-            // Activation du pipeline de post-processing
-            game.renderer.pipelines.add('CustomPipeline', new Phaser.Renderer.WebGL.Pipelines.TextureTintPipeline({
-                game: game,
-                renderer: game.renderer,
-                fragShader: `
-                    precision mediump float;
-                    uniform sampler2D uMainSampler;
-                    uniform float time;
-                    varying vec2 outTexCoord;
-                    void main() {
-                        vec4 color = texture2D(uMainSampler, outTexCoord);
-                        // Ajout d'effets de post-processing
-                        color.rgb += 0.01 * sin(time + outTexCoord.x * 10.0);
-                        gl_FragColor = color;
-                    }
-                `
-            }));
-        }
-    },
+    antialias: true,
     banner: false
 };
