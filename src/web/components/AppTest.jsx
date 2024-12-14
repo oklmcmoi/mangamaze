61b5a94740e19bce8920313e27fcd5ddc9cf8b29
oklmcmoi <oklmcrypto@gmail.com>
2024-12-14 16:38:20 +0100

    Simplify React setup for debugging

---
+++ src/web/components/AppTest.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className="app-container">
+            <div className="container py-4">
+                <h1 className="text-center">MangaMaze</h1>
+                <p className="text-center">Bienvenue dans le jeu !</p>
+            </div>
+        </div>
+    );
+};
+
+export default App;
--- src/web/index.js
+++ src/web/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './components/App';
+import App from './components/AppTest';
 import './styles/main.css';
 
 const container = document.getElementById('root');
